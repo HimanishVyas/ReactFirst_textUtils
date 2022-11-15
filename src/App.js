ac7563065bfb0130ac63154b268a4f860f877069
Himanish <himanish.vyas@gmail.com>
2022-11-15 13:23:13 +0530

    till react router eroor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 
 import './App.css';
+import About from './componentes/About';
 import Nevbar from './componentes/Nevbar';
 import TextForm from './componentes/TextForm';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
   <>
+  <Router>
     <Nevbar />
-    <div className='container'>
-          <TextForm heading = "Enter your text to analyze"/>
-    </div>
+      <div className='container'>
+            <Switch>
+              <Route path="/">
+                <TextForm heading = "Enter your text to analyze"/>
+              </Route>
+              <Route path="/about">
+                <About />
+              </Route>
+            </Switch>
+      </div>
+  </Router>
+      
+    
   </>
   );
 }
--- src/componentes/Nevbar.js
+++ src/componentes/Nevbar.js
@@ -12,10 +12,10 @@ export default function Nevbar() {
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item ">
-        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+        <link className="nav-link" to="/">Home <span className="sr-only">(current)</span></link>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="/">About</a>
+        <link className="nav-link" to="/about">About</link>
       </li>
       
     </ul>
